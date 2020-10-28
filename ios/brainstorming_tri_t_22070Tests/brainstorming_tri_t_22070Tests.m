40235ff6ea9f3f34155f26fb94bb2d947a4fd3e3
Crowdbotics <team@crowdbotics.com>
2020-10-28 21:45:48 +0000

    Initial commit.

---
+++ ios/brainstorming_tri_t_22070Tests/brainstorming_tri_t_22070Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface brainstorming_tri_t_22070 : XCTestCase
+
+@end
+
+@implementation brainstorming_tri_t_22070
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
